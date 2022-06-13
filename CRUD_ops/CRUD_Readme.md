444e3190d8d70c3713b9abf0b55965f9886f1dbd
prbehere93 <prbehere93@gmail.com>
2022-06-13 05:29:06 +0000

    added a Readme.md to the CRUD ops folder

---
+++ CRUD_ops/CRUD_Readme.md
@@ -0,0 +1,9 @@
+This folder contains operations for the creation, reading, updating and deleting (CRUD) bigquery datasets, tables and GCS buckets and objects.
+
+For now the different parameters of the scripts have been hardcoded. Also, the scripts must be executed in this particular order - 
+
+1. Create
+2. Read
+3. Update
+4. Delete
+
--- pyspark/write_avro_to_bigquery.py
+++ pyspark/write_avro_to_bigquery.py
@@ -4,15 +4,22 @@ import pyspark.sql.functions as F
 
 spark = SparkSession.builder.appName('test').getOrCreate()
 uri = "gs://test-bucket-1173/twitter.avro"
-df = spark.read.format("avro").load(uri).select(F.col('username'))
-
 gcs_bucket = 'dataproc-bucket-42'
 
+df = spark.read.format("bigquery").option('project','advance-rush-349804').option('table','test_dataset_42.test-table-4') \
+  .load().select(F.col('username'))
+
+# df.createOrReplaceTempView('df') #create a temp view for running sql
+# columns = ['username']
+# newRow = spark.createDataFrame([("pratyush"),("pratyush2"),("prb3")])
+# new_df = spark.sql('INSERT INTO df(username) VALUES("pratyush"),("pratyush2"),("pratyush3") ')
+
+# df.union(newRow)
 
 # Saving the data to BigQuery
 df.write.format('bigquery').option(
     'table', 'test_dataset_42.test_spark_output').option(
-        "temporaryGcsBucket", gcs_bucket).mode(
-            'overwrite').save()
+        "temporaryGcsBucket", gcs_bucket).mode( #mentioning a temp bucket is necessary otherwise the job does not execute
+            'append').save()
 
 # gcloud dataproc jobs submit pyspark pyspark/write_avro_to_bigquery.py --jars=gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.23.2.jar --cluster example-cluster --region us-central1 --properties spark.jars.packages='org.apache.spark:spark-avro_2.12:2.4.1'
