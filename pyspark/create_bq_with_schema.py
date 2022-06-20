8e3161481402628c79de11c87c27442271a400dc
prbehere93 <prbehere93@gmail.com>
2022-06-20 05:28:51 +0000

    basic crud scripts for pyspark and dataflow

---
+++ pyspark/create_bq_with_schema.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType, DateType
+import pyspark.sql.functions as F
+
+
+
+spark = SparkSession.builder.appName('test').getOrCreate()
+gcs_bucket = 'dataproc-bucket-42'
+
+
+schema = StructType() \
+      .add("RecordNumber",IntegerType(),True) \
+      .add("TaxReturnsFiled",StringType(),True) \
+      .add("EstimatedPopulation",IntegerType(),True) \
+      .add("TotalWages",IntegerType(),True) \
+      .add("Notes",StringType(),True) \
+      .add("Date",DateType(),True)    
+
+emp_RDD = spark.sparkContext.emptyRDD()
+
+df = spark.createDataFrame(data=emp_RDD,schema=schema)
+
+# change the mode to 'append' to add new data to an existing bq table
+df.write.format('bigquery').option(
+    'table', 'test_dataset_42.test_output_420090').option(
+        "temporaryGcsBucket", gcs_bucket).mode( #mentioning a temp bucket is necessary otherwise the job does not execute
+            'overwrite').save()
+
+# gcloud dataproc jobs submit pyspark test_bq.py --jars=gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.23.2.jar --cluster example-cluster --region us-central1 --properties spark.jars.packages='org.apache.spark:spark-avro_2.12:2.4.1'
--- pyspark/insert_row_bq.py
+++ pyspark/insert_row_bq.py
@@ -1,21 +1,28 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import SQLContext
+from pyspark.sql import Row
 import pyspark.sql.functions as F
+from pyspark.sql.types import StructType,StructField, StringType, IntegerType, DateType
+
+"""To do this we need to construct a new 'Rows' of data then convert them 
+to a dataframe which is then appended to an existing BQ table. The schema of the 
+rows has to match the BQ table, you can also explicitly mention the schema. 
+"""
 
 spark = SparkSession.builder.appName('test').getOrCreate()
 gcs_bucket = 'dataproc-bucket-42'
 
-df = spark.read.format("bigquery").option(
-    'project','advance-rush-349804').option(
-        'table','test_dataset_42.transaction_demo').load()
+# schema = StructType() \
+#       .add("username",StringType(),True)
 
-newRow = spark.createDataFrame([(4,5,7)], columns)
-appended = df.union(newRow)
+row = [Row(username = 'Pratyush')]
+# newRow = spark.createDataFrame(row,schema)
+newRow = spark.createDataFrame(row)
 
 # change the mode to 'append' to add new data to an existing bq table
-df.write.format('bigquery').option(
-    'table', 'test_dataset_42.test_spark_output4').option(
+newRow.write.format('bigquery').option(
+    'table', 'test_dataset_42.test_spark_output').option(
         "temporaryGcsBucket", gcs_bucket).mode( #mentioning a temp bucket is necessary otherwise the job does not execute
-            'overwrite').save()
+            'append').save()
 
-# gcloud dataproc jobs submit pyspark pyspark/write_to_bigquery.py --jars=gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.23.2.jar --cluster example-cluster --region us-central1 --properties spark.jars.packages='org.apache.spark:spark-avro_2.12:2.4.1'
+# gcloud dataproc jobs submit pyspark insert_row_bq --jars=gs://spark-lib/bigquery/spark-bigquery-with-dependencies_2.12-0.23.2.jar --cluster example-cluster --region us-central1 --properties spark.jars.packages='org.apache.spark:spark-avro_2.12:2.4.1'
